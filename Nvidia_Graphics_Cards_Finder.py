8741deecf886bb9db0636148d48c8e141730ac35
Faruq Thaqief Bin Shamsuddin <100338233+Senpai2198@users.noreply.github.com>
2022-03-04 07:19:11 +0800

    Update Nvidia_Graphics_Cards_Finder.py

--- Nvidia_Graphics_Cards_Finder.py
+++ Nvidia_Graphics_Cards_Finder.py
@@ -5,18 +5,21 @@ import pandas as pd
 #memory_size=(1, 2, 4, 6, 8, 10, 11, 12, 16, 24)
 
 nvidia=pd.read_csv("Nvidia Graphics Cards.csv")
+nvidia=nvidia.reset_index(drop=True, inplace=True)
 nvidia=nvidia.sort_values('Released Year')
 
 st.write("""
          # GRAPHICS CARD LIST FILTER
          """)
-st.text('The purpose of this application is to let user choose the specification they require from a graphics')
-st.text('either for their graphics intensive jobs or hobby.')
+st.text('         The purpose of this application is to let user choose the specification they require from')
+st.text('a graphics either for their graphics intensive jobs or hobby.')
 st.text('')
-st.text('It simple to use, just move the sliders in the sidebar and the graphics card list will be filtered.')
+st.text('         It simple to use, just move the sliders in the sidebar and the graphics card list will be filtered.')
 
 st.text('Database Source: https://www.techpowerup.com/gpu-specs/')
 ##########################################################################################    
+
+st.sidebar.text('Move the sliders to filter the list')
 
 memory = st.sidebar.slider('Select Lowest Memory Size (in Gb):', 1,24,1)
     
