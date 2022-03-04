336811d9faf6f6d8443ab9f319351bb5f936929e
Faruq Thaqief Bin Shamsuddin <100338233+Senpai2198@users.noreply.github.com>
2022-03-04 08:03:32 +0800

    Update Nvidia_Graphics_Cards_Finder.py

--- Nvidia_Graphics_Cards_Finder.py
+++ Nvidia_Graphics_Cards_Finder.py
@@ -49,4 +49,4 @@ else:
     st.table(nvidia)
 #########################################################################################
 
-st.bar_chart(nvidia) #,x='Product Name', y='Memory (Gb)', rot=0)
+st.bar_chart(nvidia, width='Product Name', height='Memory (Gb)', rot=0)
