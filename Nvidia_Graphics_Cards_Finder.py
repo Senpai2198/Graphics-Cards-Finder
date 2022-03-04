855601a297d3bd662893b8dc0f59d27c5d44e847
Faruq Thaqief Bin Shamsuddin <100338233+Senpai2198@users.noreply.github.com>
2022-03-04 14:13:47 +0800

    Update Nvidia_Graphics_Cards_Finder.py

--- Nvidia_Graphics_Cards_Finder.py
+++ Nvidia_Graphics_Cards_Finder.py
@@ -25,17 +25,17 @@ st.sidebar.write('Move the sliders to filter the list:')
 
 type_GPC = st.sidebar.selectbox('Select NVIDIA and/or AMD:', NvidiaorAMD, 0)
 
-year = st.sidebar.checkbox('Sort Year', st.help=help_year)
+year = st.sidebar.checkbox('Sort Year')
 if year:
         GPC=GPC.sort_values('Released Year')
 
-memory = st.sidebar.slider('Select Lowest Memory Size (in Gb):', 1,24,1, st.help=help_memory)
+memory = st.sidebar.slider('Select Lowest Memory Size (in Gb):', 1,24,1)
     
-gpu=st.sidebar.slider('Select lowest GPU Clock (in MHz):', 675,2321,675, st.help=help_gpu)
+gpu=st.sidebar.slider('Select lowest GPU Clock (in MHz):', 675,2321,675)
       
-memory_c=st.sidebar.slider('Select Lowest Memory Clock (in MHz)', 900,2248, 900, st.help=help_memory_c)
+memory_c=st.sidebar.slider('Select Lowest Memory Clock (in MHz)', 900,2248, 900)
      
-shaders=st.sidebar.slider('Select Lowest Shaders:', 192,10496,192, st.help=help_shaders)
+shaders=st.sidebar.slider('Select Lowest Shaders:', 192,10496,192)
 
 #########################################################################################
 
@@ -67,29 +67,3 @@ st.markdown('Database Source ---> [TechPowerUp: GPU Specs Database](%s)' % datab
 st.image("https://www.gpumag.com/wp-content/uploads/2020/05/computer-graphics-card.jpg")
 
 #########################################################################################
-
-help_year = '''
-Sort GPUs'released years in ascending order.
-'''.strip()
-
-help_memory = '''
-GPU VRAM holds multiple data such as vertex (geometry), terrain, mesh , texture of the games.
-If there isn't enough space in GPU memory, modern GPUs will use VRAM (a dedicated memory portion of CPU RAM).
-'''.strip()
-
-help_gpu = '''
-Also known as engine clock, GPU clock speed indicates how fast the cores of a graphics processing unit (GPU) are.
-The function of these cores is to render graphics; therefore, the higher the GPU clock speed, the faster the processing.
-'''.strip()
-
-help_memory_c = '''
-The memory clock is the speed of the VRAM on the GPU.
-'''.strip()
-
-help_shaders = '''
-A shader is a piece of code that is executed on the Graphics Processing Unit (GPU), usually found on a graphics card, 
-to manipulate an image before it is drawn to the screen. Shaders allow for various kinds of rendering effect, ranging 
-from adding an X-Ray view to adding cartoony outlines to rendering output.
-'''.strip()
-
-###########################################################################################
